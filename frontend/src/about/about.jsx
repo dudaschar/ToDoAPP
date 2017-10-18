b7bebc6b9069ddf49f3422a8efab25cf09038538
eduardascharnhorst <eduardascharnhorst@gmail.com>
2017-10-18 11:10:42 -0300

    Problema no handleSearch

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small="Nós"></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem</p>
+        <h2>Missão</h2>
+        <p>Lorem Ipsum</p>
+    </div>
+)
